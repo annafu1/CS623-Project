c7cbd1a93401946a1f31639d70849b0ee7e558ce
Anna <annahuang068@gmail.com>
2024-05-02 13:20:03 -0400

    made adjustments to the code

--- code_1.py
+++ code_1.py
@@ -25,24 +25,32 @@ try:
         WHERE stock.prod_id = 'p1'
     """)
 
-     # Delete p1 from the product id in the Product table
+    # Delete p1 from the product id in the Product table
     cur.execute("""
         DELETE FROM product
         WHERE product.prod_id = 'p1'
     """)
 
-    #Insert the new pp1 to Product table
+    # Insert the new pp1 to Product table
     cur.execute("""
         INSERT INTO product (prod_id, pname, price)
-        VALUES ('pp1', 'tape', 2.5) 
+        VALUES ('pp1', 'tape', 2.5)
     """)
-    
-    #Insert the new pp1 to Stock table
+
+    # Update the prod_id in the product table
     cur.execute("""
-                UPDATE stock
-    SET prod_id = 'pp1'
-    WHERE prod_id = 'p1'
-                """)
+        UPDATE product
+        SET prod_id = 'pp1'
+        WHERE prod_id = 'p1'
+    """)
+
+    # Insert the new pp1 to Stock table
+    cur.execute("""
+        INSERT INTO stock (prod_id, dep_id, quantity)
+        VALUES ('pp1', 'd1', 100000),
+               ('pp1', 'd2', -100),
+               ('pp1', 'd4', 12000)
+    """)
 
     # Commit the transaction if everything is successful
     conn.commit()
@@ -54,6 +62,7 @@ except (Exception, psycopg2.DatabaseError) as e:
 
 finally:
     if conn:
+        # Commit the transaction if everything is successful
         conn.commit()
         cur.close()
         conn.close()
